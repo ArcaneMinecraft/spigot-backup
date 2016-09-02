03a7f970d2917e919767c171693961b12b1303e3
Braden Arestides <braden@arestides.com>
2016-09-01 21:30:20 -0400

    Adds config file, Script reads config info to dict

--- backup.py
+++ backup.py
@@ -1,3 +1,12 @@
 #!/usr/bin/env python3
+import yaml
 
-SERVER_ROOT = 
+CONFIG_DIR = 'config/config.yaml'
+config = {}
+
+with open(CONFIG_DIR, 'r') as stream:
+    try:
+        config = (yaml.load(stream))
+    except:
+        print(exc)
+
