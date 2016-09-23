529a5e2db1b39ecac025e8373261c2dff30c0133
Braden Arestides <braden@arestides.com>
2016-09-22 21:07:15 -0400

    Adds sending over sftp capability.
    Also changes to myconfig.yaml for testing purposes.

--- backup.py
+++ backup.py
@@ -4,14 +4,15 @@ import logging
 import os.path
 import tarfile
 import time
-import ftplib
+import paramiko
 
-CONFIG_DIR = "myconfig.yaml"
-config = {}
 
 logging.getLogger().setLevel(logging.INFO)
-logging.info("Reading config")
 
+#Load config
+CONFIG_DIR = "myconfig.yaml"
+config = {}
+logging.info("Reading config")
 with open(CONFIG_DIR, "r") as stream:
     try:
         config = (yaml.load(stream))
@@ -23,14 +24,25 @@ WORLD_DATA_PATH = os.path.join(config["server-root"], config["main-world-name"])
 FTP_HOST = config["ftp-server-host"]
 FTP_USER = config["ftp-server-host"]
 FTP_PASSWORD = config["ftp-password"]
+FTP_PORT = config["ftp-port"]
+REMOTE_PATH =  config["remote-path"]
 
+#Specify which files to backup
 backup_files = [WORLD_DATA_PATH, WORLD_DATA_PATH + "_nether", WORLD_DATA_PATH + "_the_end"]
 
-backup_filename = "arcane-daily-" + time.strftime("%Y-%m-%d-%H%M") + ".tar.gz"
+#create backup filename using standard date format YYYY-MM-DD
+backup_filepath = "backups/arcane-daily-" + time.strftime("%Y-%m-%d-%H") + ".tar.gz"
 
-backup_tarball = tarfile.open(backup_filename, "w:gz")
+logging.info("Generating archive")
+backup_tarball = tarfile.open(backup_filepath, "w:gz")
 for backup_file in backup_files: 
     backup_tarball.add(backup_file, arcname=os.path.basename(backup_file))
 backup_tarball.close()
 
 
+#setup SFTP connection with paramiko module
+transport = paramiko.Transport((FTP_HOST, FTP_PORT))
+transport.connect(username = FTP_USER, password = FTP_PASSWORD)
+
+sftp = paramiko.SFTPClient.from_transport(transport)
+sftp.put(backup_filepath)
